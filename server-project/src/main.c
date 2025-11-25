0410603552d081e5a5ff19003a8c6170acb784f5
ManiacHead <104504990+ManiacHead@users.noreply.github.com>
2025-11-25 15:24:05 +0100

    Add files via upload

--- server-project/src/main.c
+++ server-project/src/main.c
@@ -7,30 +7,13 @@
  * portable across Windows, Linux and macOS.
  */
 
-#if defined WIN32
-#include <winsock.h>
-#else
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#define closesocket close
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "protocol.h"
 
-#define NO_ERROR 0
 
-void clearwinsock() {
-#if defined WIN32
-	WSACleanup();
-#endif
-}
+const char* supported_cities[] = {"Bari","Roma","Milano","Napoli","Torino","Palermo","Genova","Bologna","Firenze","Venezia"};
+
 
 int main(int argc, char *argv[]) {
 
@@ -47,32 +30,144 @@ int main(int argc, char *argv[]) {
 #endif
 
 	int my_socket;
+	my_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	// TODO: Create socket
-	// my_socket = socket(...);
+	if (my_socket < 0) {
+		printf("socket creation failed.\n");
+		return -1;
+	}
 
-	// TODO: Configure server address
-	// struct sockaddr_in server_addr;
-	// server_addr.sin_family = AF_INET;
-	// server_addr.sin_port = htons(SERVER_PORT);
-	// server_addr.sin_addr.s_addr = INADDR_ANY;
+	//Assegazione indirizzo
+	struct sockaddr_in sock_add;
+	sock_add.sin_family = AF_INET;
+	sock_add.sin_addr.s_addr = inet_addr( "127.0.0.1" );
+	sock_add.sin_port = htons(SERVER_PORT);
 
-	// TODO: Bind socket
-	// bind(...);
+	if(bind(my_socket, &sock_add, sizeof(sock_add)) < 0)
+	{
+		printf("bind() failed.\n");
+		closesocket(my_socket);
+		return -1;
+	}
 
-	// TODO: Set socket to listen
-	// listen(...);
+	//Listening Initialization
+	if (listen (my_socket, QLEN) < 0) {
+		printf("listen() failed.\n");
+		closesocket(my_socket);
+		return -1;
+	}
 
-	// TODO: Implement connection acceptance loop
-	// while (1) {
-	//     int client_socket = accept(...);
-	//     // Handle client communication
-	//     closesocket(client_socket);
-	// }
+	//Main Server Loop
+	struct sockaddr_in client_add;
+	int client_socket;
+	int client_len;
+	printf( "Waiting for a client to connect...\n");
+	while (1) {
+	client_len = sizeof(client_add); //set the size of the client address
+	if ((client_socket=accept(my_socket, (struct sockaddr *)&client_add, &client_len)) < 0 )
+	{
+		printf("accept() failed.\n");
+		closesocket(my_socket);
+		clearwinsock();
+		return -1;
+	}
 
+	// Ricezione dati dal client
+	weather_request_t request;
+	int n = recv(client_socket, (char*)&request, sizeof(request), 0);
+	if (n <= 0) {
+		printf("Errore di ricevimento dal client\n");
+		closesocket(client_socket);
+	}
+	printf("Richiesta '%c %s' dal client IP %s\n", request.type, request.city, inet_ntoa(client_add.sin_addr));
+
+	// Preparazione ed elaborazione risposta
+	weather_response_t response;
+	memset(&response, 0, sizeof(response));
+
+	if (request.type != 't' && request.type != 'h' && request.type != 'w' && request.type != 'p'){
+		response.status = 2;
+	}
+	else if (!is_supported_city(request.city)) {
+		response.status = 1;
+	}
+	else {
+		response.status = 0;
+		response.type = request.type;
+		switch (request.type) {
+		case 't':
+			{
+				response.value = get_temperature();
+				break;
+			}
+		case 'h':
+			{
+				response.value = get_humidity();
+				break;
+			}
+		case 'w':
+			{
+				response.value = get_wind();
+				break;
+			}
+		case 'p':
+			{
+				response.value = get_pressure();
+				break;
+			}
+		}
+	}
+
+	// Invio dati al Client
+	if (send(client_socket, (char*)&response, sizeof(response), 0) != sizeof(response))
+	{
+		printf("Errore: messaggio di risposta non recapitato.\n");
+	}
+
+	closesocket(client_socket);
+	}
 	printf("Server terminated.\n");
 
 	closesocket(my_socket);
 	clearwinsock();
 	return 0;
-} // main end
+}
+
+
+int is_supported_city(const char* city) {
+    for (int i = 0; i < 10; i++) {
+        const char* c = supported_cities[i];
+        int match = 1;
+
+        size_t len_c = strlen(c);
+        size_t len_city = strlen(city);
+        if (len_c != len_city) continue;
+
+        for (size_t j = 0; j < len_c; j++) {
+            if (tolower(c[j]) != tolower(city[j])) {
+                match = 0;
+                break;
+            }
+        }
+        if (match) return 1;
+    }
+    return 0;
+}
+
+
+float get_temperature(void)
+{
+	return -10.0f + (rand() / (float)RAND_MAX) * 50.0f;
+}
+float get_humidity(void)
+{
+	return 20.0f + (rand() / (float)RAND_MAX) * 80.0f;
+}
+float get_wind(void)
+{
+	return (rand() / (float)RAND_MAX) * 100.0f;
+}
+float get_pressure(void)
+{
+	return 950.0f + (rand() / (float)RAND_MAX) * 100.0f;
+}
--- server-project/src/protocol.h
+++ server-project/src/protocol.h
@@ -4,6 +4,18 @@
  * Server header file
  * Definitions, constants and function prototypes for the server
  */
+#if defined WIN32
+#include <winsock.h>
+#endif
+#include <string.h>
+#include <ctype.h>s
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#define closesocket close
 
 #ifndef PROTOCOL_H_
 #define PROTOCOL_H_
@@ -13,12 +25,38 @@
 #define BUFFER_SIZE 512    // Buffer size for messages
 #define QUEUE_SIZE 5       // Size of pending connections queue
 
-// Function prototypes
-// Add here the signatures of the functions implemented by students
 
-/*
- * Example function to implement:
- * int handle_client(int client_socket);
- */
+#define QLEN 6
+#define NO_ERROR 0
+
+#define MAX_CITY_LEN 64
+
+
+void clearwinsock() {
+#if defined WIN32
+	WSACleanup();
+#endif
+}
+
+//Strutture Dati
+typedef struct {
+    char type;              // 't', 'h', 'w', 'p'
+    char city[MAX_CITY_LEN]; // Nome città
+} weather_request_t;
+
+typedef struct {
+    unsigned int status;  // Codice di stato
+    char type;            // Echo del tipo richiesto
+    float value;          // Valore del dato meteo
+} weather_response_t;
+
+
+//Prototipi funzioni
+int is_supported_city(const char* city);
+float get_temperature(void);    // -10.0 / 40.0 °C
+float get_humidity(void);       // 20.0 / 100.0 %
+float get_wind(void);           // 0.0 / 100.0 km/h
+float get_pressure(void);       // 950.0 / 1050.0 hPa
+
 
 #endif /* PROTOCOL_H_ */
